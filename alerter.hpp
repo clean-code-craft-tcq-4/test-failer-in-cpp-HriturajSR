837317b32ce5f501eea1bf4a0cfb74e6375f5f4d
HriturajSR <105203856+HriturajSR@users.noreply.github.com>
2022-08-28 17:44:17 +0530

    Update alerter.hpp

--- alerter.hpp
+++ alerter.hpp
@@ -1,20 +1,15 @@
 #include <iostream>
+#include <assert.h>
 
-int alertFailureCount = 0;
+namespace alerter
+{
+    class Alerter
+    {
 
-int networkAlertStub(float celcius) {
-    if(celcius > 200) {
-        std::cout << "ALERT: Temperature is " << celcius << " celcius. Raising alert!\n";
-        return 500;
-    }
-    return 200;
-}
-
-void alertInCelcius(float farenheit, int (*networkAlertStub)(float)) {
-    float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode = networkAlertStub(celcius);
-
-    if (returnCode != 200) {
-        alertFailureCount += 1;
-    }
+    public:
+        void alertInCelcius(float farenheit);
+        float farenheitToCelcius(float farenheit);
+        int networkAlertStub(float celcius);
+        int alertFailureCount = 0;
+    };
 }
